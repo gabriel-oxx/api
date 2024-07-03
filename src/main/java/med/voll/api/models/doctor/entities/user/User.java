6cae011d13184b95034581f75a5090e51014a0b9
Gabriel <gabrielcsilvasg@gmail.com>
2024-07-03 13:41:26 -0300

    Adicionar migração para a tabela de usuários

---
+++ src/main/java/med/voll/api/models/doctor/entities/user/User.java
@@ -0,0 +1,21 @@
+package med.voll.api.models.doctor.entities.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String userName;
+	private String password;
+}
---
+++ src/main/resources/db/migrations/V4__create-table-users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+(
+    id       bigint       not null auto_increment,
+    userName varchar(100) not null,
+    password varchar(255) not null,
+    primary key (id)
+
+);
