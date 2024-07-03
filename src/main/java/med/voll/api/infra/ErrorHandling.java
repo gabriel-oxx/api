819b7b23ff8f60b61682c6448e22e1f89afc311a
Gabriel <gabrielcsilvasg@gmail.com>
2024-07-03 09:11:25 -0300

    Adicionar tratamento de erro para requisição incorreta

--- src/main/java/med/voll/api/infra/ErrorHandling.java
+++ src/main/java/med/voll/api/infra/ErrorHandling.java
@@ -2,14 +2,32 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandling {
 	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity handleError404(){
+	public ResponseEntity handleError404() {
 		return ResponseEntity.notFound().build();
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> handleError400(MethodArgumentNotValidException ex) {
+		var errors = ex.getFieldErrors();
+		return ResponseEntity.badRequest().body(errors.stream().map(ValidationError::new).toList());
+	}
+
+	private record ValidationError(String field, String message) {
+
+public ValidationError(FieldError error) {
+	this(
+			error.getField(),
+			error.getDefaultMessage()
+	);
+}
 	}
 }
 
