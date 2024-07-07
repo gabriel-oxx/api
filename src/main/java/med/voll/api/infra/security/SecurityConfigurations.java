57f6c13b60c2baff8bc0bb6466d23c9bef79057a
Gabriel <gabrielcsilvasg@gmail.com>
2024-07-07 07:16:43 -0300

    SecurityConfigurations

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,7 +1,10 @@
 package med.voll.api.infra.security;
 
+import med.voll.api.infra.security.SecurityFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,12 +13,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
-
+@EnableWebSecurity(debug = true)
 public class SecurityConfigurations {
-	//private SecurityFilter springSecurity;
+	@Autowired
+	private SecurityFilter securityFilter;
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		/*
@@ -27,10 +31,18 @@ public class SecurityConfigurations {
 		return
 				http.csrf(csrf -> csrf.disable())
 						.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+						/*
 						.authorizeHttpRequests(req -> {
 							req.requestMatchers("/login").permitAll();
 							req.anyRequest().authenticated();
 						})
+						.build();
+						*/
+						.authorizeHttpRequests(authz -> authz
+								                                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+								                                .anyRequest().authenticated()
+						)
+						.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
 						.build();
 	}
 
@@ -40,7 +52,7 @@ public class SecurityConfigurations {
 	}
 
 	@Bean
-	PasswordEncoder passwordEncoder() {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 }
